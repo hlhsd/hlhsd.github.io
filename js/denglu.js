446848060affaaf901f77fa1a9b55c431961c7f9
石乐志 <57130363+hlhsd@users.noreply.github.com>
2019-11-18 20:12:25 +0800

    Add files via upload

---
+++ js/denglu.js
@@ -0,0 +1,13 @@
+{
+	let name = document.getElementById("name");
+    let pwd = document.getElementById("pwd");
+
+name.addEventListener("click",function(  ){    
+		name.style.color="#000000"	
+});
+	
+pwd.addEventListener("click",function(  ){    
+		pwd.style.color="#000000"	
+});
+	
+}
